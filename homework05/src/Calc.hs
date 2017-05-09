6a438971b24029c12478b56c20f4f7072164f18b
Mauricio Fierro <mfierro@stackbuilders.com>
2017-05-09 12:37:57 -0500

    Avance y prueba de git

--- homework05/src/Calc.hs
+++ homework05/src/Calc.hs
@@ -58,9 +58,43 @@ reify = id
 newtype MinMax  = MinMax Integer deriving (Eq, Show)
 newtype Mod7    = Mod7 Integer deriving (Eq, Show)
 
+class Exp a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Exp ExprT where
+  lit = ExprT.Lit
+  add = ExprT.Add 
+  mul = ExprT.Mul 
+
+instance Exp Integer where
+  lit x = x
+  add = (+)
+  mul = (*)
+
+instance Exp Bool where
+  lit x
+    | x > 0     = True
+    | otherwise = False
+  add = (||)
+  mul = (&&)
+
+instance Exp MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Exp Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+
 ----------------------------------------------------------------------
 -- Exercise 5 (do this OR exercise 6)
 ----------------------------------------------------------------------
+
+instance Exp Program where
 
 compile :: String -> Maybe Program
 compile = undefined
