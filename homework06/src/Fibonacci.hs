2146f488d072127c9efb6f9ab8ca966e9abf2aa6
Mauricio Fierro <mfierro@stackbuilders.com>
2017-05-19 11:20:26 -0500

    Finished Homework 06 minus optionals and ruler using interleave

--- homework06/src/Fibonacci.hs
+++ homework06/src/Fibonacci.hs
@@ -7,33 +7,43 @@
 
 module Fibonacci where
 
+import Data.Foldable
 ----------------------------------------------------------------------
 -- Exercise 1
 ----------------------------------------------------------------------
 
 fib :: Integer -> Integer
-fib = undefined
+fib x
+  | x == 0 = 0
+  | x == 1 = 1
+  | otherwise = generate x [0,1]
+  where
+      generate x' acc
+        | length acc ==  fromIntegral x' = last acc
+        | otherwise = generate x' (acc ++ [sum . take 2 $ reverse acc])
 
 fibs1 :: [Integer]
-fibs1 = undefined
-
+fibs1 = map fib [0..]
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = map fib [0..]
 
 
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
 
-data Stream a
+data Stream a = Stream a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show x = show . take 20 $ streamToList x
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Stream a s) = a : streamToList s
 
 
 ----------------------------------------------------------------------
@@ -41,13 +51,13 @@ streamToList = undefined
 ----------------------------------------------------------------------
 
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat s = Stream s (streamRepeat s)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Stream s s') = Stream (f s) (streamMap f s')
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f x = Stream (f x) (streamFromSeed f (f x))
 
 
 ----------------------------------------------------------------------
@@ -55,12 +65,21 @@ streamFromSeed = undefined
 ----------------------------------------------------------------------
 
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = streamMap (gPower 6) nats
+  where
+    gPower :: Integer -> Integer -> Integer
+    gPower n x
+      | x `mod` 2^n == 0 = n
+      | otherwise = gPower (n - 1) x
 
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Stream a a') (Stream b b') = Stream a (Stream b (interleaveStreams a' b'))
 
+ruler' :: Stream Integer
+ruler' = undefined
 ----------------------------------------------------------------------
 -- Exercise 6 (Optional)
 ----------------------------------------------------------------------
