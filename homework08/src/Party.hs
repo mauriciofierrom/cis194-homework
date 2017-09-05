3c0768723c75b24b221a006e13fe7145249fef53
Mauricio Fierro <mauriciofierrom@gmail.com>
2017-09-05 17:54:11 -0500

    Avance

--- homework08/src/Party.hs
+++ homework08/src/Party.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 ----------------------------------------------------------------------
 -- |
 --
@@ -9,30 +11,49 @@ module Party where
 
 import Employee
 import Data.Tree
+import Data.Monoid ((<>))
 
 ----------------------------------------------------------------------
 -- Exercise 1
 ----------------------------------------------------------------------
 
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend gl@(GL _ fun) gl'@(GL _ fun')
+    | fun >= fun' = gl
+    | otherwise = gl'
+
 glCons :: Employee -> GuestList -> GuestList
-glCons = undefined
+glCons Emp{..} (GL eList fun) = GL (Emp empName empFun:eList) (fun + empFun)
+
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun = undefined
+moreFun gl gl' = gl <> gl'
 
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
+-- treeFold :: Monoid b => (a -> b) -> Tree a -> b
+-- treeFold :: Monoid b => (a -> b) -> Tree a -> b
+-- treeFold f (Node root []) = f root
+-- treeFold f (Node root subForest) = f root <> foldMap (treeFold f) subForest
 
+foldTree :: (a -> [b] -> b) -> Tree a -> b
+foldTree f = go where
+    go (Node x ts) = f x (map go ts)
+
+treeFold :: Monoid b => (a -> b) -> Tree a -> b
+treeFold f (Node root []) = f root
+treeFold f (Node root subForest) = f root <> mconcat (fmap (treeFold f) subForest)
 
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel = undefined
+nextLevel _ gls = (,) (mconcat $ map fst gls) (mconcat $ map snd gls)
 
 
 ----------------------------------------------------------------------
@@ -40,8 +61,10 @@ nextLevel = undefined
 ----------------------------------------------------------------------
 
 maxFun :: Tree Employee -> GuestList
-maxFun = undefined
-
+-- maxFun t = fst (foldIt t) <> snd (foldIt t)
+maxFun t = uncurry (<>) (foldIt t)
+  where
+    foldIt = foldTree nextLevel
 
 ----------------------------------------------------------------------
 -- Exercise 5
