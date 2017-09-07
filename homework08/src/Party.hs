4f9c53ab5d6619f825132aca8e56a971c7da0b6e
Mauricio Fierro <mauriciofierrom@gmail.com>
2017-09-07 18:01:01 -0500

    Finished Week 8

--- homework08/src/Party.hs
+++ homework08/src/Party.hs
@@ -11,24 +11,34 @@ module Party where
 
 import Employee
 import Data.Tree
+import Data.List ( union, unzip )
 import Data.Monoid ((<>))
 
 ----------------------------------------------------------------------
 -- Exercise 1
 ----------------------------------------------------------------------
 
+-- instance Monoid GuestList where
+--   mempty = GL [] 0
+--   mappend gl@(GL _ fun) gl'@(GL _ fun')
+--     | fun >= fun' = gl
+--     | otherwise = gl'
+
 instance Monoid GuestList where
   mempty = GL [] 0
-  mappend gl@(GL _ fun) gl'@(GL _ fun')
-    | fun >= fun' = gl
-    | otherwise = gl'
+  mappend (GL emp fun) gl'@(GL emp' fun') = GL interGl funSum
+    where
+      interGl= emp `union` emp'
+      funSum = foldr (\x acc -> acc + empFun x) 0 interGl
 
 glCons :: Employee -> GuestList -> GuestList
 glCons Emp{..} (GL eList fun) = GL (Emp empName empFun:eList) (fun + empFun)
 
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun gl gl' = gl <> gl'
+moreFun gl@(GL _ stfu) gl'@(GL _ stfu')
+  | stfu >= stfu' = gl
+  | otherwise = gl'
 
 
 ----------------------------------------------------------------------
@@ -46,23 +56,23 @@ foldTree f = go where
 
 treeFold :: Monoid b => (a -> b) -> Tree a -> b
 treeFold f (Node root []) = f root
-treeFold f (Node root subForest) = f root <> mconcat (fmap (treeFold f) subForest)
+treeFold f (Node root subForest) = f root <> mconcat (map (treeFold f) subForest)
 
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
-
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel _ gls = (,) (mconcat $ map fst gls) (mconcat $ map snd gls)
-
+nextLevel emp gts = (withBoss, withoutBoss)
+  where
+    withBoss = glCons emp $ mconcat $ (snd . unzip) gts
+    withoutBoss = mconcat $ (fst . unzip) gts
 
 ----------------------------------------------------------------------
 -- Exercise 4
 ----------------------------------------------------------------------
 
 maxFun :: Tree Employee -> GuestList
--- maxFun t = fst (foldIt t) <> snd (foldIt t)
-maxFun t = uncurry (<>) (foldIt t)
+maxFun t = uncurry moreFun (foldIt t)
   where
     foldIt = foldTree nextLevel
 
@@ -71,4 +81,6 @@ maxFun t = uncurry (<>) (foldIt t)
 ----------------------------------------------------------------------
 
 main :: IO ()
-main = undefined
+main = do
+  companyText <- readFile "src/company.txt"
+  print $ maxFun (read companyText)
